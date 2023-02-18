337073943459bd2ac2772d65317d1d71662eaef9
Leaptail <52641374+Leaptail@users.noreply.github.com>
2023-02-18 18:21:42 +0800

    Readermain done for txt files
    Readermain is done! can read books from txt and tell you the letters and frequency of letters. next will be to read from other file types, like pdf, word and the like.

--- ReaderMain.C
+++ ReaderMain.C
@@ -1,23 +1,48 @@
 #include <stdio.h>
+
+
+int Letterreader(char testlttr)
+{
+    int lttrno=0;
+	char placeholder = '0';
+    FILE*in=fopen("F:\\Programming\\github C\\BookCharReader\\Booksample.txt","r");
+	placeholder = getc(in);
+	while(placeholder != EOF)
+	{
+		if(placeholder == testlttr || placeholder == testlttr - 32) // found way to check for upperlower case --
+		{
+		     lttrno++;
+		}
+		placeholder = getc(in);
+	}
+    fclose(in);
+    return lttrno;
+}
+
 // lets start with smth that reads all the letter e's
 int main()
 {
     int totalletters=0;
     int totale=0;
     double percentage;
-    char es = '0';
-    FILE*in=fopen("Booksample.txt","r");
-    es=getc(in);
-    while(es != EOF)
+    int Alphabetter[26];
+    for(int h=0; h<26; h++) //checks a-z
     {
-        totalletters++;
-        if(es == 'e') //this is somehow counting all characters, no clue how it works.
-        {
-            totale++;
-        }
-        es=getc(in);
+        char ltrhold = 'a'+ h;
+        Alphabetter[h] = Letterreader(ltrhold);
+        totalletters = totalletters+Alphabetter[h];
     }
-    percentage = 100*((double)totale / totalletters);
-    printf("Total number of Es is %d. Total Chars: %d. Percent: %3.2f.",totale,totalletters,percentage);
-    fclose(in);
+        //printing the values for every letter and frequency
+    printf("Lttr / Total / Freq\n");
+        
+    for(int j=0;j<26;j++)
+    {
+    	totale=Alphabetter[j];
+    	percentage = 100*((double)totale/totalletters);
+    	printf("%c     %4d     %3.2f\n",j + 'a',totale,percentage);
+     }
+     printf("Total letters: %d\n", totalletters);
 }
+
+
+
--- Readernamev1_3.C
+++ Readernamev1_3.C
@@ -1,4 +1,24 @@
 #include <stdio.h>
+
+
+int Letterreader(char testlttr)
+{
+    int lttrno=0;
+	char placeholder = '0';
+    FILE*in=fopen("F:\\Programming\\github C\\BookCharReader\\Booksample.txt","r");
+	placeholder = getc(in);
+	while(placeholder != EOF)
+	{
+		if(placeholder == testlttr ) // found way to check for upperlower case --|| placeholder == testlttr - 32
+		{
+		     lttrno++;
+		}
+		placeholder = getc(in);
+	}
+    fclose(in);
+    return lttrno;
+}
+
 // lets start with smth that reads all the letter e's
 int main()
 {
@@ -8,8 +28,8 @@ int main()
     int Alphabetter[26];
     for(int h=0; h<26; h++) //checks a-z
     {
-        char ltrhold = 'a' + h;
-        Letterreader(ltrhold, &Alphabetter[h]);
+        char ltrhold = 'a'+ h;
+        Alphabetter[h] = Letterreader(ltrhold);
         totalletters = totalletters+Alphabetter[h];
     }
         //printing the values for every letter and frequency
@@ -19,24 +39,10 @@ int main()
     {
     	totale=Alphabetter[j];
     	percentage = 100*((double)totale/totalletters);
-    	printf("%c %4d %3.2f\n",j + 'a',totale,percentage);
+    	printf("%c     %4d     %3.2f\n",j + 'a',totale,percentage);
      }
      printf("Total letters: %d\n", totalletters);
-
-     fclose(in);
 }
 
 
-int Letterreader(char testlttr, int *lttrno)
-{
-	char placeholder = '0';
-	placeholder = getc(in);
-	while(placeholder != EOF)
-	{
-		if(placeholder == testlttr || placeholder == testlttr - 32) // found way to check for upperlower case
-		{
-		     *lttrno++;
-		}
-		placeholder = getc(in);
-	}
-}
+
